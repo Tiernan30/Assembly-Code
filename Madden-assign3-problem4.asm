ecadda643877a14a1c7c909051ab952febd9a7ec
Jared Madden <tier.cerelli@hotmail.com>
2016-06-13 16:46:40 -0600

    Changes to assignment3, problem 4
    Topic

--- Madden-assign3-problem4.asm
+++ Madden-assign3-problem4.asm
@@ -9,23 +9,15 @@ INCLUDE Irvine32.inc
 .data
 bigEndian Byte 12h, 34h, 56h, 78h		;The bigEndian array
 littleEndian DWORD ?					;The littleEndian array
+
 	
 .code
 	main proc
-	mov EAX, 0							;Initialize EAX to 0
-	mov ECX, lengthof bigEndian			;move the length of bigEndian to ECX
-	mov EAX, ECX						;Move ECX to EAX
-	sub EAX, TYPE bigEndian				;Subtract from EAX the type of an element of bigEndian(in this case 1)
-	mov ESI, EAX						;Move the new value in EAX to ESI
-		L1:								;Loop to go backwards through bigEndian and move the elements to littleEndian
-			movzx EAX, bigEndian[ESI]	;Move the current element in bigEndian into EAX(starting with the last)
-			Call WriteInt				;Print screen for error checking
-			Call Crlf
-			mov littleEndian, EAX		;Move the element to the littleEndian array
-			sub ESI, TYPE bigEndian		;Decrement our pointer by the type of the bigEndian array
-			Loop L1						;Decrement ECX and continue the loop
+	mov eax, DWORD ptr bigEndian		;The ptr command automatically reverses the bits when used, so
+										;the elements of bigEndian are reversed and stored in eax
+	mov littleEndian, EAX				;Then we just move those elements into the littleEndian array
+	
 		
-		Call WaitMsg					;For error checking, brings a window awaiting any key to be pressed to continue
-		exit	
+		exit
 	main endp
 	end main
